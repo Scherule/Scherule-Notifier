984d90f82570bafae9990594dd4a3f58f6bedf3b
Grzegorz Gurgul <gurgul.grzegorz@gmail.com>
2017-06-23 23:05:17 +0200

    Adding service discovery

--- src/main/kotlin/com/scherule/notifying/NotifyingVerticle.kt
+++ src/main/kotlin/com/scherule/notifying/NotifyingVerticle.kt
@@ -1,15 +1,30 @@
 package com.scherule.notifying;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.servicediscovery.ServiceDiscovery
+import io.vertx.servicediscovery.ServiceDiscoveryOptions
 
 class NotifyingVerticle : AbstractVerticle() {
 
     override fun start() {
+        val discovery = ServiceDiscovery.create(vertx,
+                ServiceDiscoveryOptions()
+                        .setAnnounceAddress("scherule-announcing")
+                        .setName("notifying"))
+
         vertx.createHttpServer()
                 .requestHandler({
                     it.response().end("Hello Vert.x!")
                 })
-        .listen(8080);
+                .listen(config().getInteger("http.port", 8081), {
+                    if (it.succeeded()) {
+                        System.out.println("Server started");
+                    } else {
+                        System.out.println("Cannot start the server: " + it.cause());
+                    }
+                })
+
+        discovery.close()
     }
 
 }
